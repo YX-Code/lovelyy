571944556d17e2ac3ed321b617b9d2ddf031aaf2
YX-Code <yjx6555159@126.com>
2018-12-12 17:58:11 +0800

    Create

---
+++ javase-study/src/main/java/cn/lovelyy/study/javase/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <div style="font-weight: 900">说明:</div>
+ * <p>
+ * <div style="font-weight: 900">创建时间:2018/12/12</div>
+ *
+ * @author yaojianxing
+ * @since
+ */
+package cn.lovelyy.study.javase;
